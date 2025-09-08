d6945726ead476f4d638c552e05215cc8ab448a2
aabruscato <arabrusc@syr.edu>
2025-09-07 22:31:28 -0400

    Began writing program

--- Cow_Sim.py
+++ Cow_Sim.py
@@ -1,2 +1,27 @@
-# This is the cow simulator python program
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+# Packages used ^
+# Claire O'Connor & Amelia Abruscato
+# Polonius the Projectile Cow Simulator Python Program
+
+def main(): # main function
+    print("main")
+
+# INITIAL CONDITIONS AND CONSTANTS
+mass = 1000
+a_g = 9.81
+
+h = 80
+pos = np.array([0, h])
+
+vel_ox = 4
+vel_oy = 3
+vel = np.array([vel_ox, vel_oy])
+
+C_d = 1
+
+time = 0
+del_t = 0.1
+
 
