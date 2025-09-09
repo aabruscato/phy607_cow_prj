ad96e6b3fb13bbbd14cf82f06845b5ef635a2b27
aabruscato <arabrusc@syr.edu>
2025-09-08 21:04:05 -0400

    Updated code for Polonius plunging to the ground; Added two functions and graphing the collected data.

--- Cow_Sim.py
+++ Cow_Sim.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 # Packages used ^
 # Claire O'Connor & Amelia Abruscato
 # Polonius the Projectile Cow Simulator Python Program
@@ -16,7 +15,7 @@ def main():
     # Variables
     h = 80
     Cd = 1  # drag constant
-    del_t = 0.0001  # time step
+    delt = 0.0001  # time step
     vox = 5
     voy = 6
 
@@ -31,15 +30,78 @@ def main():
         F = np.array([Fg[0] + Fd[0], Fg[1] + Fd[1]])
         return F
 
-    #def moment_later(pos, vel, del_t):
+    def moment_later(pos, vel, F, del_t):
+        a = np.array([F[0]/mass,F[1]/mass])
+        post = np.array([pos[0] + vel[0] * delt, pos[1] + vel[1] * delt])
+        velt = np.array([vel[0] + a[0] * delt, vel[1] + a[1] * delt])
+        return post, velt
 
+    def energy(pos, vel):
+        KE = 0.5 * mass * (vel[0]**2 + vel[1]**2)
+        PE = mass * g * pos[1]
+        E = KE + PE
+        return KE, PE, E
 
-    #def energy(pos, vel):
+    # DATA COLLECTION LISTS
+    POSITION = []
+    VELOCITY = []
+    TIME = []
+    ENERGY = []
 
+    while pos[1] > 0:
 
+        # Calculating force, position, velocity, and energy of Polonius @ an instant
+        F = total_force(vel)
+        pos, vel = moment_later(pos, vel, F, delt)
+        KE, PE, E = energy(pos, vel)
 
+        # STORING DATA
+        POSITION.append(np.linalg.norm(pos))
+        VELOCITY.append(np.linalg.norm(vel))
+        TIME.append(time)
+        ENERGY.append(E)
 
+        time += delt
 
+        print(f"Time: {time:.4f}, Position: {pos}, Velocity: {vel}, KE: {KE:.2f}, PE: {PE:.2f}, Total Energy: {E:.2f}")
+
+    Position = np.array(POSITION)
+    Velocity = np.array(VELOCITY)
+    Time = np.array(TIME)
+    Energy = np.array(ENERGY)
+
+    # PLOTTING DATA
+    plt.figure(figsize=(12, 10))
+
+    # Position vs Time
+    plt.subplot(3, 1, 1)
+    plt.plot(Time, Position, label='Position', color='blue')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Position (m)')
+    plt.title('Position vs Time')
+    plt.legend()
+
+    # Velocity vs Time
+    plt.subplot(3, 1, 2)
+    plt.plot(Time, Velocity, label='Velocity', color='red')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Velocity (m/s)')
+    plt.title('Velocity vs Time')
+    plt.legend()
+
+    # Energy vs Time
+    plt.subplot(3, 1, 3)
+    plt.plot(Time, Energy, label='Total Energy', color='green')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Energy (J)')
+    plt.title('Energy vs Time')
+    plt.ticklabel_format(style='sci', axis='y', scilimits=(-3, 3))
+    plt.legend()
+
+    plt.tight_layout()
+
+    #plt.show()
+    plt.savefig('cow_simulation_plot.png', dpi=300)
 
 if __name__ == "__main__":
     main()
